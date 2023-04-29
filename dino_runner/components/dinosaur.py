9b296c2b40d7e94bfb7c32c359427b8166654af6
MiltonGabriel007 <59025916+MiltonGabriel007@users.noreply.github.com>
2023-04-28 20:43:42 -0500

    Class 2. Moving dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -10,8 +10,23 @@ class Dinosaur:
         self.dino_rect.x = 80
         self.dino_rect.y = 310
         self.dino_run = True
+        self.step_index = 0
+
 
     def update(self):
-        pass
+        if self.dino_run:
+            self.run()
+        
+        if self.step_index > 10:
+            self.step_index = 0  
+
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = 80
+        self.dino_rect.y = 310
+        self.step_index += 1
+
